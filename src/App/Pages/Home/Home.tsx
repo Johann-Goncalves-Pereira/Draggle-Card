cbf92f4577196b725ab1e519a52b300752349cc3
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-06 18:37:05 -0300

    feat: :fire: Dragable element

--- src/App/Pages/Home/Home.tsx
+++ src/App/Pages/Home/Home.tsx
@@ -1,72 +1,113 @@
-import { useEffect, useState } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import styles from "./Home.module.scss";
 
-const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updateMousePosition = (event: {
-      clientX: number;
-      clientY: number;
-    }) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
-
-    const listener = window.addEventListener("mousemove", updateMousePosition);
-
-    listener;
-    return () => listener;
-  }, []);
-
-  return mousePosition;
+export const throttle = (f: { (event: any): void; (arg0: any): void }) => {
+  let token: number | null = null,
+    lastArgs: any[] | null = null;
+  const invoke = () => {
+    f(...lastArgs);
+    token = null;
+  };
+  const result = (...args: any[]) => {
+    lastArgs = args;
+    if (!token) {
+      token = requestAnimationFrame(invoke);
+    }
+  };
+  result.cancel = () => token && cancelAnimationFrame(token);
+  return result;
 };
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
+const id = (x: any) => x;
 
-export function useWindowDimensions() {
-  const [useWindowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+const useDraggable = ({ onDrag = id } = {}) => {
+  const [pressed, setPressed] = useState(false);
 
-  useEffect(() => {
-    function handleSize() {
-      setWindowDimensions(getWindowDimensions());
+  const position = useRef({ x: 0, y: 0 });
+  const ref = useRef();
+
+  const unsubscribe = useRef();
+  const legacyRef = useCallback((elem) => {
+    ref.current = elem;
+    if (unsubscribe.current) {
+      unsubscribe.current();
     }
-
-    const wd = window.addEventListener("resize", handleSize);
-
-    wd;
-    return () => wd;
+    if (!elem) {
+      return;
+    }
+    const handleMouseDown = (e: {
+      target: { style: { userSelect: string } };
+    }) => {
+      e.target.style.userSelect = "none";
+      setPressed(true);
+    };
+    elem.addEventListener("mousedown", handleMouseDown);
+    unsubscribe.current = () => {
+      elem.removeEventListener("mousedown", handleMouseDown);
+    };
   }, []);
 
-  return useWindowDimensions;
-}
+  useEffect(() => {
+    if (!pressed) {
+      return;
+    }
+
+    const handleMouseMove = throttle(
+      (event: { movementX: number; movementY: number }) => {
+        if (!ref.current || !position.current) {
+          return;
+        }
+        const pos = position.current;
+
+        const elem = ref.current;
+        position.current = onDrag({
+          x: pos.x + event.movementX,
+          y: pos.y + event.movementY,
+        });
+
+        // elem.style.transform = `translate(${pos.x * 2.5}px, ${pos.y * 2.5}px)`;
+        elem.style = `--posX: ${pos.x * 2}px; --posY: ${pos.y * 2}px;`;
+      }
+    );
+    const handleMouseUp = (e) => {
+      e.target.style.userSelect = "auto";
+      setPressed(false);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      handleMouseMove.cancel();
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [pressed, onDrag]);
+
+  return [legacyRef, pressed];
+};
 
 function Home() {
-  // const { height, width } = useWindowDimensions();
-  const { x, y } = useMousePosition();
-  const { height, width } = useWindowDimensions();
+  const handleDrag = useCallback(
+    ({ x, y }) => ({
+      x: Math.max(0, x),
+      y: Math.max(0, y),
+    }),
+    []
+  );
 
-  const posHandler = {
-    color: "",
-    "--posX": `${x - 300}px`,
-    "--posY": `${y - 25}px`,
-    "--width": `${width}px`,
-    "--height": `${height}px`,
-  };
-
-  // console.log(x, y);
+  const [ref, pressed] = useDraggable({
+    onDrag: handleDrag,
+  });
 
   return (
     <main className={styles.Home}>
-      <section className={styles.Home__ctnr} style={posHandler}>
+      <section
+        className={styles.Home__ctnr}
+        ref={ref}
+        // style={quickAndDirtyStyle}
+      >
         <header className={styles.Home__header} aria-labelledby="PageId">
+          {/* <p>{pressed ? "Dragging..." : "Press to drag"}</p> */}
           <div className={styles.dotsWrapper}>
             <span></span>
             <span></span>
