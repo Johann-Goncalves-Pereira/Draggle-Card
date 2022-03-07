a0db490702222071e1b642f9dc31b8aaf46e0514
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-07 14:52:09 -0300

    feat: :art: change styles path and new plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./App/Pages/Home/Home";
-import "./App/Styles/_reset.scss";
+import "./Styles/_reset.scss";
+import "./Styles/_base.scss";
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import globalStyle from "@originjs/vite-plugin-global-style";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    globalStyle({
+      sourcePath: "./src/Styles",
+      lessEnabled: false,
+    }),
+  ],
+});
